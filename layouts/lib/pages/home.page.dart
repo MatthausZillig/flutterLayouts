093d11cba64df519d370777691204bcc324ce192
MatthausZillig <matthauszigillig@hotmail.com>
2020-04-21 23:23:24 -0300

    feature: SOLID

--- layouts/lib/pages/home.page.dart
+++ layouts/lib/pages/home.page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:layouts/pages/product.page.dart';
+import 'package:layouts/widgets/category/caegory-list.widget.dart';
+import 'package:layouts/widgets/search-box.widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -15,7 +17,7 @@ class HomePage extends StatelessWidget {
               SizedBox(
                 height: 60,
               ),
-              search(),
+              SearchBox(),
               SizedBox(
                 height: 30,
               ),
@@ -30,7 +32,7 @@ class HomePage extends StatelessWidget {
               ),
               Container(
                 height: 90,
-                child: categoryList(),
+                child: CategoryList(),
               ),
               SizedBox(
                 height: 30,
@@ -63,84 +65,6 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
-
-Widget search() {
-  return Container(
-    height: 60,
-    padding: EdgeInsets.only(
-      left: 20,
-    ),
-    decoration: BoxDecoration(
-      color: Colors.black.withOpacity(0.1),
-      borderRadius: BorderRadius.all(
-        Radius.circular(128),
-      ),
-    ),
-    child: Row(
-      children: <Widget>[
-        Icon(Icons.search),
-        Container(
-          width: 300,
-          padding: EdgeInsets.only(left: 10),
-          child: TextFormField(
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              labelText: "Search...",
-              labelStyle: TextStyle(
-                color: Colors.blue,
-                fontWeight: FontWeight.w300,
-                fontSize: 16,
-              ),
-            ),
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.blue,
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-Widget categoryList() {
-  return ListView(
-    scrollDirection: Axis.horizontal,
-    children: <Widget>[
-      categoryItem(),
-      categoryItem(),
-      categoryItem(),
-      categoryItem(),
-      categoryItem(),
-      categoryItem(),
-    ],
-  );
-}
-
-Widget categoryItem() {
-  return Container(
-    width: 70,
-    height: 70,
-    margin: EdgeInsets.all(10),
-    padding: EdgeInsets.all(10),
-    decoration: BoxDecoration(
-      color: Colors.white,
-      boxShadow: [
-        new BoxShadow(
-          color: Colors.black12,
-          offset: new Offset(1, 1),
-          blurRadius: 5,
-          spreadRadius: 2,
-        ),
-      ],
-      borderRadius: BorderRadius.all(
-        Radius.circular(64),
-      ),
-    ),
-    child: Image.asset("assets/Icon_Devices.png"),
-  );
 }
 
 Widget productList(BuildContext context) {
